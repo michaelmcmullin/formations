3366333cf1f90adce0298e53a5a316ab6c723302
Michael McMullin <blueark@outlook.com>
2017-02-17 07:29:09 +0000

    Replace 'watch' as default grunt task
    At the moment, I'm bouncing around different command line tools and
    watch is kind of getting in the way. It's still being loaded though,
    because I think it will be useful later on.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,5 +32,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.registerTask('default',['sass', 'cssmin']);
 }
